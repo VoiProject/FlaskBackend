6e66d4620f05e24946e4b9890f06cf1b302fd948
Sergei <isara.isara8@gmail.com>
2020-11-23 21:08:56 +0300

    Add docker configuration

---
+++ backend/views.py
@@ -0,0 +1,93 @@
+from flask import Flask, jsonify, abort
+from backend import app
+
+users = [
+    {
+        'id': 1,
+        "login": "Mykyta",
+        "hash": "1234",
+        "posts": [1, 2]
+    },
+    {
+        'id': 2,
+        "login": "Sergei",
+        "hash": "1234",
+        "posts": [3, 4]
+    },
+]
+
+posts = [
+    {
+        'id': 1,
+        'title': 'post1 title',
+        'short_description': 'post1 description',
+        'long_description': 'post1 long description',
+    },
+    {
+        'id': 2,
+        'title': 'post2 title',
+        'short_description': 'post2 description',
+        'long_description': 'post2 long description'
+    },
+    {
+        'id': 3,
+        'title': 'post4 title',
+        'short_description': 'post3 description',
+        'long_description': 'post3 long description'
+    },
+    {
+        'id': 4,
+        'title': 'post4 title',
+        'short_description': 'post4 description',
+        'long_description': 'post4 long description'
+    },
+]
+
+
+@app.route('/', methods=['GET'])
+def site_map():
+    res = str(app.url_map)
+    return res
+
+
+@app.route('/api/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = list(filter(lambda u: u['id'] == user_id, users))
+    print('go here')
+    if len(user) == 0:
+        abort(404)
+    return jsonify({'user': user[0]})
+
+
+@app.route('/api/post/<int:post_id>', methods=['GET'])
+def get_post(post_id):
+    post = list(filter(lambda u: u['id'] == post_id, posts))
+    if len(post) == 0:
+        abort(404)
+    return jsonify({'post': post[0]})
+
+
+@app.route('/api/posts/user/<int:user_id>', methods=['GET'])
+def get_user_posts(user_id):
+    user = list(filter(lambda u: u['id'] == user_id, users))
+    if len(user) == 0:
+        abort(404)
+    user = user[0]
+    user_posts = user['posts']
+
+    user_posts = list(filter(lambda p: p['id'] in user_posts, posts))
+    if len(user_posts) == 0:
+        abort(404)
+    return jsonify({'user_posts': user_posts})
+
+
+@app.route('/api/feed/<int:user_id>', methods=['GET'])
+def get_user_feed(user_id):
+    user = list(filter(lambda u: u['id'] == user_id, users))
+    if len(user) == 0:
+        abort(404)
+    user = user[0]
+    user_posts = user['posts']
+
+    user_feed = list(filter(lambda p: p['id'] not in user_posts, posts))
+    return jsonify({'user_feed': user_feed})
