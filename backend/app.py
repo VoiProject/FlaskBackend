4a789fca4828ae4ea8c7f745be574190928869d0
Sergei <isara.isara8@gmail.com>
2020-11-23 19:43:34 +0300

    Update api

--- backend/app.py
+++ backend/app.py
@@ -7,13 +7,13 @@ users = [
         'id': 1,
         "login": "Mykyta",
         "hash": "1234",
-        "posts": ["1", "2"]
+        "posts": [1, 2]
     },
     {
         'id': 2,
         "login": "Sergei",
         "hash": "1234",
-        "posts": ["3"]
+        "posts": [3, 4]
     },
 ]
 
@@ -22,7 +22,7 @@ posts = [
         'id': 1,
         'title': 'post1 title',
         'short_description': 'post1 description',
-        'long_description': 'post1 long description'
+        'long_description': 'post1 long description',
     },
     {
         'id': 2,
@@ -43,6 +43,12 @@ posts = [
         'long_description': 'post4 long description'
     },
 ]
+
+
+@app.route('/', methods=['GET'])
+def site_map():
+    res = str(app.url_map)
+    return res
 
 
 @app.route('/api/user/<int:user_id>', methods=['GET'])
@@ -70,10 +76,22 @@ def get_user_posts(user_id):
     user = user[0]
     user_posts = user['posts']
 
-    user_posts = list(filter(lambda u: u['id'] in user_posts, posts))
+    user_posts = list(filter(lambda p: p['id'] in user_posts, posts))
     if len(user_posts) == 0:
         abort(404)
     return jsonify({'user_posts': user_posts})
+
+
+@app.route('/api/feed/<int:user_id>', methods=['GET'])
+def get_user_feed(user_id):
+    user = list(filter(lambda u: u['id'] == user_id, users))
+    if len(user) == 0:
+        abort(404)
+    user = user[0]
+    user_posts = user['posts']
+
+    user_feed = list(filter(lambda p: p['id'] not in user_posts, posts))
+    return jsonify({'user_feed': user_feed})
 
 
 if __name__ == '__main__':
