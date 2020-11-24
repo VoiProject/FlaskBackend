152d237c3ea0111cfa660f6d962f36c797f386df
Sergei <isara.isara8@gmail.com>
2020-11-24 08:33:10 +0300

    Update cors

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+# cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
 from backend import views
