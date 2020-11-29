df48b4e27f1e97be5e6a4aaccc99eaf4df9f1bf7
Sergei <isara.isara8@gmail.com>
2020-11-30 02:50:31 +0300

    Add elasticsearch sample

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,13 +1,31 @@
 import os
+from datetime import datetime
 
+from elasticsearch import Elasticsearch
 from flask import Flask
 from flask_cors import CORS
-from flask_elasticsearch import FlaskElasticsearch
 
 app = Flask(__name__, static_url_path='/Frontend/')
-es = FlaskElasticsearch(app)
+es = Elasticsearch([{'host': os.environ['ELASTIC_HOST'], 'port': os.environ['ELASTIC_PORT']}])
 
 print("Elasticsearch config: ", es)
+
+doc = {
+    'author': 'kimchy',
+    'text': 'Elasticsearch: cool. bonsai cool.',
+    'timestamp': datetime.now(),
+}
+res = es.index(index="test-index", id=1, body=doc)
+print(res['result'])
+
+res = es.get(index="test-index", id=1)
+print(res['_source'])
+
+es.indices.refresh(index="test-index")
+
+res = es.search(index="test-index", body={"query": {"match_all": {}}})
+print(res)
+
 
 app.secret_key = os.urandom(24)
 app.config['UPLOAD_FOLDER'] = "../Frontend"
