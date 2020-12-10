9f1b8bad37ad33bf4b31a078b7ed7cf2a059e29b
nikitosoleil <nikiolei@gmail.com>
2020-12-10 21:56:44 +0200

    Work on login / registration tests

--- test_all.py
+++ test_all.py
@@ -51,8 +51,7 @@ def register(client, creds):
     return client.post('/api/register', data=json.dumps(creds))
 
 
-def logout(client, user_data):
-    set_cookies(client, user_data)
+def logout(client):
     return client.post('/api/logout')
 
 
@@ -63,39 +62,50 @@ def login(client, creds):
 @pytest.fixture
 def registration_data(client):
     response = register(client, sample_creds[0])
-    return json.loads(response.data)
+    user_data = json.loads(response.data)
+    set_cookies(client, user_data)
+    return user_data
 
 
 def test_registration(client):
     response = register(client, sample_wrong_creds[0])
     assert response.status_code == 404
 
+    # 4.1.1.2
     response = register(client, sample_creds[0])
     assert response.status_code == 200
     data = json.loads(response.data)
     assert 'user_id' in data and 'session_token' in data
     assert data['user_id'] == 1 and isinstance(data['session_token'], str)
 
+    # 4.1.1.5
     response = register(client, sample_creds[0])
     assert response.status_code == 409
 
 
 def test_logout(client, registration_data):
-    response = logout(client, sample_user_data[0])
-    assert response.status_code == 401
-
-    response = logout(client, registration_data)
+    response = logout(client)
     assert response.status_code == 200
     data = json.loads(response.data)
     assert data['status'] == 'OK'
 
+    response = logout(client)
+    assert response.status_code == 401
+
 
 def test_login(client, registration_data):
-    logout(client, registration_data)
+    logout(client)
 
+    # 4.1.1.3
     response = login(client, sample_creds[1])
     assert response.status_code == 404
 
+    # 4.1.1.4
+    response = login(client, {'login': sample_creds[0]['login'],
+                              'pwd_hash': sample_creds[1]['pwd_hash']})
+    assert response.status_code == 404
+
+    # 4.1.1.1
     response = login(client, sample_creds[0])
     assert response.status_code == 200
     data = json.loads(response.data)
